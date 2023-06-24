f84adac7f644624bb22a96428d43345689e44bce
Anthony <blacknebula236@gmail.com>
2023-06-24 20:14:01 +0930

    FIrst project commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import Header from './components/Header.vue';
+import Sidebar from './components/Sidebar.vue';
+</script>
+
+<template>
+  <div id="app">
+    <!-- <Header></Header> -->
+    <Sidebar></Sidebar>
+  </div>
+</template>
+
+<style lang="scss">
+:root {
+	--primary: #4ad4de;
+	--primary-alt: #22a5c5;
+	--grey: #64748b;
+	--dark: #1e293b;
+	--dark-alt: #334155;
+	--light: #f1f5f9;
+	--sidebar-width: 300px;
+}
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	font-family: 'Fira sans', sans-serif;
+}
+body {
+	background: var(--light);
+}
+button {
+	cursor: pointer;
+	appearance: none;
+	border: none;
+	outline: none;
+	background: none;
+}
+.app {
+	display: flex;
+	main {
+		flex: 1 1 0;
+		padding: 2rem;
+		@media (max-width: 1024px) {
+			padding-left: 6rem;
+		}
+	}
+}
+</style>
