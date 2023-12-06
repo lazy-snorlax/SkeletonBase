4a68d15396f039dad2dde0913887887da8560b71
Anthony <blacknebula236@gmail.com>
2023-12-06 15:19:20 +1030

    user model + login controller + api routes

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use App\Notifications\ResetPasswordNotification;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -41,4 +43,20 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Password attribute caster.
+     */
+    public function password(): Attribute
+    {
+        return new Attribute(set: fn ($value) => $value ? bcrypt($value) : null);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function sendPasswordResetNotification($token): void
+    {
+        $this->notify(new ResetPasswordNotification($token));
+    }
 }
--- api/routes/api.php
+++ api/routes/api.php
@@ -1,19 +1,29 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', fn () => null);
+
+// Login/Logout =======================================================
+Route::post('/login', [Controllers\LoginController::class, 'store']);
+Route::post('/logout', [Controllers\LoginController::class, 'destroy']);
+
+// TODO: Email Verification =================================================
+
+
+// TODO: Password Reset =====================================================
+Route::post('/password/forgot', Controllers\Password\ForgotPasswordController::class);
+Route::post('/password/reset', Controllers\Password\ResetPasswordController::class);
+
+
+// TODO: Global Lookups =====================================================
+
+
+// Authenticated ============================================================
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', Controllers\LoggedInController::class);
+    Route::put('/user', Controllers\UpdateAccountDetailsController::class);
+    Route::put('/user/password', Controllers\UpdatePasswordController::class);
 });
